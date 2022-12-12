950c1890a68566301c50532f6e076538a29365ab
[Students] Ivans Sadovnikovs <03DPISadovnikovs@rvt.lv>
2022-12-12 13:20:37 +0200

    db

--- database.php
+++ database.php
@@ -8,7 +8,6 @@ echo "Connected Successfully";
 CloseCon($conn);
 ?>
 <head>
-    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
@@ -22,7 +21,24 @@ CloseCon($conn);
     <li id="navigating"><a href="contact.php">CONTACT</a></li>
     <li id="navigating"><a href="https://github.com/ZaDivan/Majaslappa" target="_blank".>GITHUB</a></li>
     <li id="navigating"><a href="database.php">DATABASE</a></li>
-</nav>    
-    <h1 id="logo">Ivans Sadovnikovs</h1>
+</nav>      
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <br><input type="text" placeholder="name" name="name"><br>
+    <br><input type="text" placeholder="email" name="email"><br>
+    <br><input type="text" placeholder="message" name="message"><br>
+    <br><input type="submit">
+<?php
+    $name => $_POST["name"]
+    $email => $_POST["email"]
+    $message => $_POST["message"]
+    $sql = "INSERT INTO MyGuests (name,email,message)
+VALUES ($name, $email, $message)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
 </body>
 </html>
